60b7dcc748df8c402c3b66b1171f5eb07031e5e3
Khojiakbarbe <121534562+Khojiakbarbe@users.noreply.github.com>
2023-08-16 15:23:45 +0500

    completed

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -39,15 +39,15 @@ function Cart() {
                 <hr className='my-5' />
                 <div className='grid lg:grid-cols-2 gap-10'>
                     {
-                        state?.map((cart, i) => <div key={i} className='flex gap-10 dark:text-white my-5'>
-                            <img src={cart?.img} className='h-[400px] w-[300px] rounded-lg' alt="" />
+                        state?.map((cart, i) => <div key={i} className='sm:flex gap-3 md:gap-10 dark:text-white my-5'>
+                            <img src={cart?.img} className='w-[200px] h-[200px] sm:h-[400px] sm:w-[300px] rounded-lg' alt="" />
                             <div>
-                                <h1 className='text-4xl font-bold'>{cart.title}</h1>
+                                <h1 className='text-2xl md:text-4xl font-bold'>{cart.title}</h1>
                                 <p className='text-xl font-bold my-5'>{cart.company}</p>
                                 <p>Price:${cart.price / 100}</p>
                                 <p className='flex items-center my-5 gap-4'>Color: <button className='w-5 h-5 rounded-full' style={{ background: cart.color }} /></p>
                                 <p>Amount</p>
-                                <div className='flex justify-center bg-blue-700 text-2xl text-white rounded-lg p-1 gap-10 mb-10'>
+                                <div className='flex justify-center bg-blue-700 text-2xl text-white rounded-lg p-1 gap-3 md:gap-10 mb-10'>
                                     <button onClick={() => decrease(cart.id, cart.color)} className='flex items-center'><AiOutlineMinusCircle/></button>
                                     <span>{cart.count}</span>
                                     <button className='flex items-center ' onClick={() => increase(cart.id, cart.color)}><AiOutlinePlusCircle/></button>
